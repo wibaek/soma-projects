d9b8facd870a6aadf054c1b6cd444392697e8030
Wi Baek Park <34394229+wibaek@users.noreply.github.com>
2025-05-10 01:04:10 +0900

    Merge pull request #1 from wibaek/feature/admin
    feat: 어드민 페이지 추가

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import { getAuth } from "firebase/auth";
 // Firebase 설정
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +14,6 @@ const firebaseConfig = {
 // Firebase 초기화 (중복 초기화 방지)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };

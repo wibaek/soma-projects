f8b863a2300a3b130a99214aa9f61c62257fd5e6
Wibaek Park <devmuromi@gmail.com>
2025-05-09 23:41:17 +0900

    feat: 로그인 페이지 추가

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+  signInWithEmailAndPassword,
+  getIdTokenResult,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../../lib/firebase";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { user } = await signInWithEmailAndPassword(auth, email, password);
+    const tokenResult = await getIdTokenResult(user, true);
+    if (!tokenResult.claims.admin) {
+      await signOut(auth);
+      alert("관리자 권한이 없습니다.");
+      return;
+    }
+    const res = await fetch("/api/sessionLogin", {
+      method: "POST",
+      body: JSON.stringify({ idToken: tokenResult.token }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (res.ok) router.replace("/admin");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            관리자 로그인
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={onSubmit}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                이메일 주소
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="관리자 이메일"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                비밀번호
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="비밀번호"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              로그인
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
